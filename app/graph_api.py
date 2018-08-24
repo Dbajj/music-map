312d53c6a6a6d9f14d88d557a4f78e488f34c70a
Daniel Bajj <daniel.bajj@gmail.com>
2018-08-23 23:15:49 -0700

    Switch to pytest and add mock neo4j database setup/teardown in test_graph_api.py

--- app/graph_api.py
+++ app/graph_api.py
@@ -1,33 +1,28 @@
 from neo4j.v1 import GraphDatabase
+import pdb
 
 class GraphAdapter():
     def __init__(self, uri, user, password):
-        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+        self._driver = GraphDatabase.driver(uri)
+
+
 
     def close(self):
         self._driver.close()
 
 
-    
     def get_artist_by_id(self,artist_id):
         """ 
         Return artist object from database corresponding to given id
         if no artist found, returns None
         """
-        with self._driver.session() as session:
-            result = list(session.run(f"MATCH(n:Artist {{artistId: \"{artist_id}\"}}) return(n)"))
-            if len(result) == 0:
-                return None
-            else:
-                artist_node = result[0]['n']
-                return Artist(artist_node.get('name'), artist_node.get('artistId'))
-
-
+            #artist_node = result[0]['n']
+            #return Artist(artist_node.get('name'), artist_node.get('artistId'))
 
 class Artist():
     def __init__(self, name, artist_id):
         self.name = name
         self.id = artist_id
-            
+
 
 
--- test/test_graph_api.py
+++ test/test_graph_api.py
@@ -1,18 +1,53 @@
-import unittest
 import pdb
+import pytest
 from app import graph_api
+import docker
+import os
+import time
 
-TEST_URI = "bolt://127.0.0.1:7687"
-TEST_USER = "neo4j"
-TEST_PASSWORD = "***REMOVED***"
+TEST_URI = "bolt://127.0.0.1:7688"
+SECOND = 1000000000
+CHECKTIMEOUT = 1
 
-class TestGraphApi(unittest.TestCase):
+
+@pytest.fixture
+def test_api():
+    working_dir = os.getcwd()
+    client = docker.from_env()
+    container = None
+
+    try:
+        container = client.containers.get("neo4j_test")
+        if (container.status == 'exited'):
+            container.start()
+    except:
+        client.containers.run("neo4j:3.0",
+                ports={'7474/tcp':7478, '7687/tcp':7688},
+                volumes={f"{working_dir}/neo4j-test/data" : {'bind': '/data', 'mode': 'rw' },
+                    f'{working_dir}/neo4j-test/logs': {'bind': '/logs', 'mode': 'rw'}}, 
+                detach=True,
+                environment = ["NEO4J_AUTH=none"],
+                name="neo4j_test",
+                healthcheck = {"test": "curl --fail http://127.0.0.1:7474 || exit 1",
+                    "interval": 5*SECOND,
+                    "timeout": 5*SECOND,
+                    "retries": 5},
+                )
+        container = client.containers.get("neo4j_test")
+
+
+    while(container.attrs['State']['Health']['Status'] != 'healthy'):
+        time.sleep(CHECKTIMEOUT)
+        container = client.containers.get("neo4j_test")
+
+
+    api = graph_api.GraphAdapter(TEST_URI, "neo4j", "***REMOVED***")
+    yield api
+    api.close()
+    container.kill()
     
-    def test_get_artist_by_id(self):
-        test_instance = graph_api.GraphAdapter(TEST_URI, TEST_USER, TEST_PASSWORD)
-        test_instance.close()
 
-    
-if __name__ == '__main__':
-    unittest.main()
+def test_get_artist_by_id(test_api):
+    test_api.get_artist_by_id("123451")
+
 
