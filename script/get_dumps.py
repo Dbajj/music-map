f2945cf1fde49bb656dafba230acb19555e72dba
Dbajj <daniel.bajj@gmail.com>
2018-09-04 20:08:09 -0700

    Finish download functionality on python script (currently downloads all of artists, masters and releases)

--- script/get_dumps.py
+++ script/get_dumps.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+import sys
+import requests
 import pdb
 import re
 import math
@@ -54,19 +56,58 @@ def get_download_link(html_output,dl_type):
 
     return get_latest_link(links)
 
+def download_file(link, output_name):
+    response = requests.get(link, stream=True)
+    response.raise_for_status()
+    with open(output_name, 'wb') as f:
+        down = 0
+        length = int(response.headers.get('content-length'))
+        for block in response.iter_content(1024*1024*5):
+            down += len(block)
+            done = int(50 * down / length )
+            sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+            sys.stdout.flush()
+            f.write(block)
+        print(f"File saved as \"{output_name}\"")
 
 
+
+
+
+
+
+# Setup selenium headless driver with options
 options = Options()
 options.set_headless(headless=True)
 driver = webdriver.Chrome(chrome_options=options)
 driver.implicitly_wait(2)
+
+# Connect to the data dump landing page
 driver.get('http://data.discogs.com')
 wait = WebDriverWait(driver,10)
 wait.until(expected_conditions.presence_of_element_located((By.ID,'listing')))
 assert 'Discogs Data' in driver.title
 
+# Grab the link for the latest year dump
 year_link = get_year_link(driver.page_source)
 driver.get(year_link)
 wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,'#listing a')))
 
+# Download latest artist dump
 latest = get_download_link(driver.page_source,'artists')
+print("Downloading artist dump from " + latest)
+download_file(latest,'artists.xml.gz')
+
+# Download latest master dump
+latest = get_download_link(driver.page_source,'masters')
+print("Downloading masters dump from " + latest)
+download_file(latest,'masters.xml.gz')
+
+# Download latest releases dump
+latest = get_download_link(driver.page_source,'releases')
+print("Downloading masters dump from " + latest)
+download_file(latest,'releases.xml.gz')
+
+
+
+
